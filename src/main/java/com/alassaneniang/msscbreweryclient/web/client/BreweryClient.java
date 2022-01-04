1d442e11d3c913755eb99299d05684644d3119fc
Alassane NIANG <adniang75@gmail.com>
2022-01-04 11:06:24 +0100

    Refactoring Brewery Client (RestTemplate). closes #8

--- src/main/java/com/alassaneniang/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/alassaneniang/msscbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,7 @@
 package com.alassaneniang.msscbreweryclient.web.client;
 
 import com.alassaneniang.msscbreweryclient.web.model.BeerDTO;
+import com.alassaneniang.msscbreweryclient.web.model.CustomerDTO;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import java.util.UUID;
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
+    private final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
     private final RestTemplate restTemplate;
     private String apihost;
 
@@ -21,7 +23,7 @@ public class BreweryClient {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    // get beer by id
+    // Beer
     public BeerDTO getBeerById ( UUID beerId ) {
         return restTemplate.getForObject( apihost + BEER_PATH_V1 + beerId.toString(), BeerDTO.class );
     }
@@ -40,6 +42,23 @@ public class BreweryClient {
 
     public void setApihost ( String apihost ) {
         this.apihost = apihost;
+    }
+
+    // Customer
+    public CustomerDTO getCustomerById ( UUID id ) {
+        return restTemplate.getForObject( apihost + CUSTOMER_PATH_V1 + id, CustomerDTO.class );
+    }
+
+    public URI saveNewCustomer ( CustomerDTO customerDTO ) {
+        return restTemplate.postForLocation( apihost + CUSTOMER_PATH_V1, customerDTO );
+    }
+
+    public void updateCustomer ( UUID id, CustomerDTO customerDTO ) {
+        restTemplate.put( apihost + CUSTOMER_PATH_V1 + id.toString(), customerDTO );
+    }
+
+    public void deleteCustomer ( UUID id ) {
+        restTemplate.delete( apihost + CUSTOMER_PATH_V1 + id.toString() );
     }
 
 }
--- src/test/java/com/alassaneniang/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/alassaneniang/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package com.alassaneniang.msscbreweryclient.web.client;
 
 import com.alassaneniang.msscbreweryclient.web.model.BeerDTO;
+import com.alassaneniang.msscbreweryclient.web.model.CustomerDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ class BreweryClientTest {
     @Autowired
     BreweryClient client;
 
+    // Beer
     @Test
     void getBeerById () {
         BeerDTO beerDTO = client.getBeerById( UUID.randomUUID() );
@@ -48,5 +50,41 @@ class BreweryClientTest {
     @Test
     void testDeleteBeer () {
         client.deleteBeer( UUID.randomUUID() );
+    }
+
+    // Customer
+    @Test
+    void getCustomerById () {
+        CustomerDTO customerDTO = client.getCustomerById( UUID.randomUUID() );
+        log.info( "Customer: {}", customerDTO );
+        assertNotNull( customerDTO );
+    }
+
+    @Test
+    void testSaveNewCustomer () {
+        CustomerDTO customerDTO = CustomerDTO.builder()
+                .id( UUID.randomUUID() )
+                .name( "Test Customer" )
+                .build();
+        URI uri = client.saveNewCustomer( customerDTO );
+        assertNotNull( uri );
+        log.info( "New Customer URI: {}", uri.toString() );
+    }
+
+    @Test
+    void testUpdateCustomer () {
+        CustomerDTO customerDTO = CustomerDTO.builder()
+                .id( UUID.randomUUID() )
+                .name( "Test Customer" )
+                .build();
+        client.updateCustomer( UUID.randomUUID(), customerDTO );
+        log.info( "Update Customer: {}", customerDTO );
+    }
+
+    @Test
+    void testDeleteCustomer () {
+        UUID id = UUID.randomUUID();
+        client.deleteCustomer( id );
+        log.info( "Delete Customer with id {}", id );
     }
 }
